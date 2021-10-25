01d3ad4eb45455a7c6920da8355ebbd01858539a
Italo Magno <92609748+italomagno@users.noreply.github.com>
2021-10-25 19:36:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function putClassShow(click){
+  document.getElementById("MENU").classList.add("show")
+}
+function removeClassShow(){
+  document.getElementById("MENU").classList.remove("show")
+}
+
+
